f7d47da9d093684e43a9134e3f309b3796fd81fe
Vincent Chu <vincentchu@gmail.com>
2017-05-25 16:18:07 +0800

    Move inception_resnet_v2 into code base to experiment with diff network archs

--- lib/deeplab.py
+++ lib/deeplab.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from nets.inception import inception_resnet_v2
+from inception_resnet_v2 import inception_resnet_v2
 
 def network(imgs):
     return inception_resnet_v2(imgs)
