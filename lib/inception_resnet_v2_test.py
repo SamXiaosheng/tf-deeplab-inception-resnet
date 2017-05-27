e2a824a80adf063ce99fae5f6f9bc84c01c0341c
Vincent Chu <vincentchu@gmail.com>
2017-05-27 11:08:20 +0800

    Add test for decapitated resnet

---
+++ lib/inception_resnet_v2_test.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+from inception_resnet_v2 import inception_resnet_v2
+
+class InceptionResnetTest(tf.test.TestCase):
+    def testNetworkSetup(self):
+        images = tf.placeholder(tf.float32, shape=[10, 299, 299, 3])
+        net = inception_resnet_v2(images)
+        self.assertListEqual(net.get_shape().as_list(), [10, 35, 35, 1204])
+
+if __name__ == "__main__":
+    tf.test.main()
