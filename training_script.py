738c3000e1076bec283bd23a4f796786d23bdb07
Vincent Chu <vincentchu@gmail.com>
2017-06-04 13:59:53 -0700

    WIP

--- training_script.py
+++ training_script.py
@@ -9,31 +9,36 @@ OUT_DIR = "/tmp/deeplab"
 LIB_DIR = os.path.abspath("./lib")
 sys.path.extend([ LIB_DIR ])
 
-import numpy as np
 import tensorflow as tf
 import deeplab
 
 from pipeline import PipelineManager
+from training import average_accuracy
+from labels import to_labels
+
+TARGET_SIZE = [350, 500]
 
 os.system("rm %s" % (os.path.join(OUT_DIR, "*")))
 
 with tf.Session() as sess:
     summary_writer = tf.summary.FileWriter(OUT_DIR, graph=sess.graph)
-    manager = PipelineManager("/root/tf-deeplab-inception-resnet/DATA", "dev2.txt")
+    manager = PipelineManager("/mnt/hdd0/datasets/pascal/VOCdevkit/VOC2012", "dev.txt",
+        target_size=TARGET_SIZE)
+
     img_queue = manager.create_queues()
     manager.start_queues(sess)
 
     image_batch, ground_truth_batch = img_queue.dequeue_up_to(2, name="ImageBatchDequeue")
-    net = deeplab.network(image_batch)
-    print("NN", net.get_shape())
-    net_resized = tf.image.resize_images(net, [350, 500], method=tf.image.ResizeMethod.BILINEAR)
+    preds = deeplab.network(image_batch)
+    resized_preds = tf.image.resize_images(preds, TARGET_SIZE, method=tf.image.ResizeMethod.BILINEAR)
+    avg_accuracy = average_accuracy(to_labels(ground_truth_batch), resized_preds)
 
     sess.run([ tf.local_variables_initializer(), tf.global_variables_initializer() ])
 
     for i in range(1):
-        n_summ, nr_summ = sess.run([ net, net_resized ])
-        print("NSUMM", n_summ.shape)
-        print("NSUMM", nr_summ.shape)
+        foo = sess.run(avg_accuracy)
+        print("FOO", foo)
+
 
 
         # n, nr = sess.run([ net, net_resized ])
