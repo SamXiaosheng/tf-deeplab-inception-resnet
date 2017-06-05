39d2a9d7f7bf6b949060570d1044b0f6570309eb
Vincent Chu <vincentchu@gmail.com>
2017-06-05 00:12:16 -0700

    fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,12 @@ const exec = require('child_process').exec
 
 const Port = 9999
 const Host = 'vincentchu@localhost'
-const SrcDir = ''
+const SrcDir = '/Users/vince/workspace/tf-deeplab-inception-resnet/'
 const DestDir = '/home/vincentchu/workspace/tf-deeplab-inception-resnet'
 
 gulp.task('sync', (cb) => {
   console.log('Syncing!')
-  cmd = `rsync -avzr --include='*.py' --include='*/' --exclude='*' -e "ssh -p 9999" /Users/vince/workspace/tf-deeplab-inception-resnet/ vincentchu@localhost:/home/vincentchu/workspace/tf-deeplab-inception-resnet`
+  cmd = `rsync -avzr --include='*.py' --include='*/' --exclude='*' -e "ssh -p ${Port}" ${SrcDir} ${Host}:${DestDir}`
   exec(cmd, (err, stdout, stderr) => {
     console.log(stdout)
     cb()
