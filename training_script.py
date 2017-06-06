b135d7a663699b36f5fbfdcbfa3e1f0b6b9281bd
Vincent Chu <vincentchu@gmail.com>
2017-06-06 15:43:41 -0700

    Wow shit's working!!

--- training_script.py
+++ training_script.py
@@ -13,7 +13,7 @@ import tensorflow as tf
 import deeplab
 
 from pipeline import PipelineManager
-from training import average_accuracy
+from training import average_accuracy, cross_entropy
 from labels import to_labels
 
 TARGET_SIZE = [350, 500]
@@ -22,7 +22,7 @@ os.system("rm %s" % (os.path.join(OUT_DIR, "*")))
 
 with tf.Session() as sess:
     summary_writer = tf.summary.FileWriter(OUT_DIR, graph=sess.graph)
-    manager = PipelineManager("/root/tf-deeplab-inception-resnet/DATA", "dev2.txt",
+    manager = PipelineManager("/mnt/hdd0/datasets/pascal/VOCdevkit/VOC2012", "dev.txt",
         target_size=TARGET_SIZE)
 
     img_queue = manager.create_queues()
@@ -30,11 +30,12 @@ with tf.Session() as sess:
 
     image_batch, ground_truth_batch = img_queue.dequeue_up_to(2, name="ImageBatchDequeue")
     preds = deeplab.network(image_batch)
+    labeled_ground_truth = to_labels(ground_truth_batch)
     resized_preds = tf.image.resize_images(preds, TARGET_SIZE, method=tf.image.ResizeMethod.BILINEAR)
-    avg_accuracy = average_accuracy(to_labels(ground_truth_batch), resized_preds)
-    avg_error = 1.0 - avg_accuracy
 
-    train_step = tf.train.MomentumOptimizer(0.001, 0.9).minimize(tf.reduce_sum(preds))
+    xentropy = cross_entropy(labeled_ground_truth, resized_preds)
+    avg_accuracy = average_accuracy(labeled_ground_truth, resized_preds)
+    train_step = tf.train.MomentumOptimizer(0.001, 0.9).minimize(xentropy)
 
     sess.run([ tf.local_variables_initializer(), tf.global_variables_initializer() ])
 
