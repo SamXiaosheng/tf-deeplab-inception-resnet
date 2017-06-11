ac85f57209934b3395cc78d1d7cf9da1a133bd2b
Vincent Chu <vincentchu@gmail.com>
2017-06-11 11:56:39 -0700

    This looks like it's training! For posterity, looks like my handling of ignore pixels was at fault

--- training_script.py
+++ training_script.py
@@ -9,6 +9,7 @@ OUT_DIR = "/tmp/deeplab"
 LIB_DIR = os.path.abspath("./lib")
 sys.path.extend([ LIB_DIR ])
 
+import numpy as np
 import tensorflow as tf
 import deeplab
 
@@ -17,12 +18,12 @@ from training import average_accuracy, cross_entropy
 from labels import to_labels, to_images
 
 TARGET_SIZE = [350, 500]
-BATCH_SIZE = 10
+BATCH_SIZE = 2
 
 os.system("rm %s" % (os.path.join(OUT_DIR, "*")))
 
 def create_and_start_queues():
-    manager = PipelineManager("/mnt/hdd0/datasets/pascal/VOCdevkit/VOC2012", "train.txt",
+    manager = PipelineManager("/mnt/hdd0/datasets/pascal/VOCdevkit/VOC2012", "dev.txt",
         target_size=TARGET_SIZE, device="/cpu:0", threads=20)
 
     img_queue = manager.create_queues()
@@ -55,19 +56,75 @@ with tf.Session() as sess:
 
     avg_accuracy = average_accuracy(labeled_ground_truth, resized_preds)
     xentropy = cross_entropy(labeled_ground_truth, resized_preds)
-    train_step = tf.train.MomentumOptimizer(0.001, 0.9).minimize(xentropy)
+    reg_vars = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+    reg_losses = tf.add_n(reg_vars)
+    total_loss = xentropy + reg_losses
+
+    train_step = tf.train.MomentumOptimizer(0.00001, 0.9).minimize(total_loss)
 
     img_summaries = create_image_summaries(image_batch, ground_truth_batch, resized_preds)
-
     sess.run([ tf.local_variables_initializer(), tf.global_variables_initializer() ])
 
-    for i in range(100000):
-        acc, _ = sess.run([ avg_accuracy, train_step ])
+    for i in range(100):
+        acc, xent, _ = sess.run([ avg_accuracy, xentropy, train_step ])
+        if (i % 10 == 0):
+            print("%7d: Accuracy = %7.3f, Xentropy = %7.3f" % (i, 100 * acc, xent))
 
-        if (i % 100 == 0):
-            print("Accuracy = %7.3f" % (100 * acc))
             for summary in sess.run(img_summaries):
                 summary_writer.add_summary(summary, i)
 
     manager.stop_queues()
     summary_writer.flush()
+
+    # avg_accuracy = average_accuracy(labeled_ground_truth, resized_preds)
+    # xentropy, denom = cross_entropy(labeled_ground_truth, resized_preds)
+    # reg_vars = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+    # reg_losses = tf.add_n(reg_vars)
+    #
+    # loss_fn = xentropy + reg_losses
+    #
+    # print("REG", reg_losses)
+    #
+    # test_loss = reg_losses
+    #
+    # grads_and_vars = tf.train.MomentumOptimizer(0.001, 0.9).compute_gradients(test_loss)
+    # grads_and_vars = filter(lambda gv: gv[0] is not None, grads_and_vars)
+    #
+    # for g, v in grads_and_vars:
+    #     if (g == None):
+    #         print("NONE", g, v)
+    #
+    #
+    # grads = [ gv[0] for gv in grads_and_vars ]
+    # variables = [ gv[1] for gv in grads_and_vars ]
+    # #
+    # # img_summaries = create_image_summaries(image_batch, ground_truth_batch, resized_preds)
+    #
+    # # train_step = tf.train.MomentumOptimizer(0.00001, 0.9).minimize(test_loss)
+    # sess.run([ tf.local_variables_initializer(), tf.global_variables_initializer() ])
+    #
+    # # train_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+    # # [ print(v) for v in train_vars ]
+    #
+    # print("GRADS", grads)
+    # evaled_grads = sess.run(grads)
+    # evaled_vars = sess.run(variables)
+    # foo = zip(variables, evaled_grads)
+    #
+    # for g1, g2 in foo:
+    #     print("*********")
+    #     print("grad", g1.name)
+    #     print(g2)
+    #
+    #
+    #
+    # # for i in range(100000):
+    # #     print("TRAIN", sess.run(train_step))
+    # #     print("TEST LOSS", sess.run(test_loss))
+    # #     # acc, xent, _, _denom = sess.run([ avg_accuracy, xentropy, train_step, denom ])
+    # #     #
+    # #     # if (i % 1 == 0):
+    # #     #     print("Accuracy = %7.3f, Xentropy = %7.3f, denom = %f" % (100 * acc, xent, _denom))
+    # #
+    # #         for summary in sess.run(img_summaries):
+    # #             summary_writer.add_summary(summary, i)
