c77b4d7af4de79e5edeb2059447bbfdb9524cf37
Vincent Chu <vincentchu@gmail.com>
2017-06-12 07:30:59 -0700

    refactor main(_) to use tf.app.run(). Add model savers

--- training_script.py
+++ training_script.py
@@ -5,7 +5,6 @@ from __future__ import print_function
 import sys
 import os
 
-OUT_DIR = "/tmp/deeplab"
 LIB_DIR = os.path.abspath("./lib")
 sys.path.extend([ LIB_DIR ])
 
@@ -17,12 +16,13 @@ from pipeline import PipelineManager
 from training import average_accuracy, cross_entropy
 from labels import to_labels, to_images
 
+OUT_DIR = "/tmp/deeplab"
 TARGET_SIZE = [350, 500]
 BATCH_SIZE = 2
+STEPS = 100000
+SAVE_EVERY = 1000
 
-os.system("rm %s" % (os.path.join(OUT_DIR, "*")))
-
-def create_and_start_queues():
+def create_and_start_queues(sess):
     manager = PipelineManager("/mnt/hdd0/datasets/pascal/VOCdevkit/VOC2012", "dev.txt",
         target_size=TARGET_SIZE, device="/cpu:0", threads=20)
 
@@ -43,88 +43,52 @@ def create_image_summaries(imgs, gt, predicted):
 
         return [ im_summ, gt_summ, pred_summ ]
 
-with tf.Session() as sess:
-    summary_writer = tf.summary.FileWriter(OUT_DIR, graph=sess.graph)
+def create_savers(graph):
+    summary_writer = tf.summary.FileWriter(OUT_DIR, graph=graph)
+    saver = tf.train.Saver(var_list=tf.global_variables(), max_to_keep=10)
 
-    manager, image_batch, ground_truth_batch = create_and_start_queues()
+    return summary_writer, saver
 
-    preds = deeplab.network(image_batch)
+def save_checkpoint(step, sess, saver, summary_writer, avg_accuracy, xentropy, img_summaries):
+    _avg_accuracy, _xentropy, _img_summaries = sess.run([ avg_accuracy, xentropy, img_summaries ])
+    print("%7d: Accuracy = %7.3f, Xentropy = %7.3f" % (step, 100 * _avg_accuracy, _xentropy))
 
-    labeled_ground_truth = to_labels(ground_truth_batch)
-    resized_preds = tf.image.resize_images(preds, TARGET_SIZE,
-        method=tf.image.ResizeMethod.BILINEAR)
+    model_path = os.path.join(OUT_DIR, "model.ckpt")
+    saver.save(sess, model_path, global_step=step)
+    for summary in _img_summaries:
+        summary_writer.add_summary(summary, step)
 
-    avg_accuracy = average_accuracy(labeled_ground_truth, resized_preds)
-    xentropy = cross_entropy(labeled_ground_truth, resized_preds)
-    reg_vars = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
-    reg_losses = tf.add_n(reg_vars)
-    total_loss = xentropy + reg_losses
+def main(_):
+    os.system("rm %s" % (os.path.join(OUT_DIR, "*")))
 
-    train_step = tf.train.MomentumOptimizer(0.00001, 0.9).minimize(total_loss)
+    with tf.Session() as sess:
+        manager, image_batch, ground_truth_batch = create_and_start_queues(sess)
+        preds = deeplab.network(image_batch)
+        labeled_ground_truth = to_labels(ground_truth_batch)
+        resized_preds = tf.image.resize_images(preds, TARGET_SIZE,
+            method=tf.image.ResizeMethod.BILINEAR)
+        img_summaries = create_image_summaries(image_batch, ground_truth_batch, resized_preds)
 
-    img_summaries = create_image_summaries(image_batch, ground_truth_batch, resized_preds)
-    sess.run([ tf.local_variables_initializer(), tf.global_variables_initializer() ])
+        avg_accuracy = average_accuracy(labeled_ground_truth, resized_preds)
+        xentropy = cross_entropy(labeled_ground_truth, resized_preds)
+        reg_vars = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+        reg_losses = tf.add_n(reg_vars)
+        total_loss = xentropy + reg_losses
 
-    for i in range(100):
-        acc, xent, _ = sess.run([ avg_accuracy, xentropy, train_step ])
-        if (i % 10 == 0):
-            print("%7d: Accuracy = %7.3f, Xentropy = %7.3f" % (i, 100 * acc, xent))
+        train_step = tf.train.MomentumOptimizer(0.00001, 0.9).minimize(total_loss)
 
-            for summary in sess.run(img_summaries):
-                summary_writer.add_summary(summary, i)
+        sess.run([ tf.local_variables_initializer(), tf.global_variables_initializer() ])
+        summary_writer, saver = create_savers(sess.graph)
 
-    manager.stop_queues()
-    summary_writer.flush()
+        for i in range(STEPS):
+            sess.run(train_step)
 
-    # avg_accuracy = average_accuracy(labeled_ground_truth, resized_preds)
-    # xentropy, denom = cross_entropy(labeled_ground_truth, resized_preds)
-    # reg_vars = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
-    # reg_losses = tf.add_n(reg_vars)
-    #
-    # loss_fn = xentropy + reg_losses
-    #
-    # print("REG", reg_losses)
-    #
-    # test_loss = reg_losses
-    #
-    # grads_and_vars = tf.train.MomentumOptimizer(0.001, 0.9).compute_gradients(test_loss)
-    # grads_and_vars = filter(lambda gv: gv[0] is not None, grads_and_vars)
-    #
-    # for g, v in grads_and_vars:
-    #     if (g == None):
-    #         print("NONE", g, v)
-    #
-    #
-    # grads = [ gv[0] for gv in grads_and_vars ]
-    # variables = [ gv[1] for gv in grads_and_vars ]
-    # #
-    # # img_summaries = create_image_summaries(image_batch, ground_truth_batch, resized_preds)
-    #
-    # # train_step = tf.train.MomentumOptimizer(0.00001, 0.9).minimize(test_loss)
-    # sess.run([ tf.local_variables_initializer(), tf.global_variables_initializer() ])
-    #
-    # # train_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
-    # # [ print(v) for v in train_vars ]
-    #
-    # print("GRADS", grads)
-    # evaled_grads = sess.run(grads)
-    # evaled_vars = sess.run(variables)
-    # foo = zip(variables, evaled_grads)
-    #
-    # for g1, g2 in foo:
-    #     print("*********")
-    #     print("grad", g1.name)
-    #     print(g2)
-    #
-    #
-    #
-    # # for i in range(100000):
-    # #     print("TRAIN", sess.run(train_step))
-    # #     print("TEST LOSS", sess.run(test_loss))
-    # #     # acc, xent, _, _denom = sess.run([ avg_accuracy, xentropy, train_step, denom ])
-    # #     #
-    # #     # if (i % 1 == 0):
-    # #     #     print("Accuracy = %7.3f, Xentropy = %7.3f, denom = %f" % (100 * acc, xent, _denom))
-    # #
-    # #         for summary in sess.run(img_summaries):
-    # #             summary_writer.add_summary(summary, i)
+            if (i % SAVE_EVERY == 0):
+                save_checkpoint(i, sess, saver, summary_writer, avg_accuracy, xentropy,
+                    img_summaries)
+
+        manager.stop_queues()
+        summary_writer.flush()
+
+if __name__ == '__main__':
+    tf.app.run()
